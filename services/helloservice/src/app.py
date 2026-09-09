d9f0bb11c83f78746f20e2cc6803063295c3385f
IshKevin <carterk279@gmail.com>
2026-09-09 13:03:13 +0200

    feat: update service version to 1.1.1 in helloservice

--- services/helloservice/src/app.py
+++ services/helloservice/src/app.py
@@ -19,7 +19,7 @@ def index():
 @app.route("/version")
 def version():
     """Report the current service version."""
-    return jsonify(version="0.1.2")
+    return jsonify(version="1.1.1")
 
 
 if __name__ == "__main__":
